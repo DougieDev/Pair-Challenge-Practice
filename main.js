f5477142e1710c168fa07524054259bdc293d302
December G <dgarnetsmith@gmail.com>
2020-02-13 11:48:43 -0700

    Make nav bar sort of work

--- main.js
+++ main.js
@@ -5,11 +5,41 @@ var fourthList = document.querySelector(".fourth-list");
 var main = document.getElementsByTagName('main')[0];
 var nav = document.getElementsByTagName('nav');
 
-document.addEventListener('click', clickHandler);
+// document.addEventListener('click', clickHandler);
+//
+// function clickHandler(event) {
+//   if (event.target === firstList) {
+//     main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
+//     console.log(event.target);
+//   } else if (event.target.classList.contains('secondList')) {
+//     main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
+//     console.log(event.target);
+// } else if (event.target.classList.contains('thirdList')) {
+//   main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
+//   console.log(event.target);
+// } else if (event.target.classList.contains('fourthList')) {
+//   main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
+//   console.log(event.target);
+// }
+// }
 
-function clickHandler(event) {
-  if (event.target === firstList) {
-    main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
-    console.log(event.target)
-  }
+firstList.addEventListener('click', something);
+secondList.addEventListener('click', something1);
+thirdList.addEventListener('click', something2);
+fourthList.addEventListener('click', something3);
+
+function something() {
+  main.innerHTML = `<h2>Share Udemy with Friends!</h2>`
+}
+
+function something1() {
+  main.innerHTML = `<h2>second</h2>`
+}
+
+function something2() {
+  main.innerHTML = `<h2>third</h2>`
+}
+
+function something3() {
+  main.innerHTML = `<h2>fourth</h2>`
 }
